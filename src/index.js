aca7e4ba00d22901f62559b7c88c37249e21d063
Tam Mendera <mendera.1@osu.edu>
2022-02-25 14:56:53 -0500

    change commit message

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, Tam!');
 ReactDOM.render(element, document.getElementById('root'));
 
 
